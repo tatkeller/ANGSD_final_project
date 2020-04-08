1301bbb1ee3233fb8d289674a6fdbe8592d9dfd5
Tate Keller <tak76@curie.pbtech>
2020-04-08 17:43:48 -0400

    Feature counts

---
+++ scripts/readCounts.bash
@@ -0,0 +1,14 @@
+#! /bin/bash -l
+
+#SBATCH --partition=angsd_class
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --job-name=features
+#SBATCH --mem=20G
+
+spack load subread
+
+featureCounts -a /home/tak76/tak76/finalProject/extra/hg38/hg38.refGene.gtf \
+              -o featureCounts.txt\
+              /home/tak76/tak76/finalProject/alignments/*.bam       
+
