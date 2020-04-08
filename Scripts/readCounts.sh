db2c28fe5419d78036228046665f74686cfe2e07
Tate Andrew Keller <30704183+tatkeller@users.noreply.github.com>
2020-04-08 16:35:03 -0400

    Delete readCounts.sh

--- Scripts/readCounts.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-spack load subread
-
-featureCounts -a /home/tak76/tak76/finalProject/hg38/hg38.refGene.gtf \
-              -o featureCounts.txt\
-              /home/tak76/tak76/finalProject/alignments/*.bam       
-
